80c36bcf5d86fb0d18fe98cfb9800179de1e1e9a
iuuukhueeee <nguyenducthien9@gmail.com>
2023-03-27 22:00:36 +0700

    setup apolloServer

---
+++ prisma/migrations/20230318154213_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,49 @@
 import { PrismaClient } from '@prisma/client'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import cors from 'cors'
+import { HelloResolver } from './resolvers/hello'
+import { json } from 'body-parser'
+
+import express from 'express'
 
 const prisma = new PrismaClient()
 
-const main = async () => {}
+const typeDefs = `#graphql
+
+  type Query {
+    hello: String
+  }
+`
+
+const main = async () => {
+  const app = express()
+  const apolloServer = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers: [HelloResolver] }),
+    introspection: true
+  })
+
+  await apolloServer.start()
+
+  // app.get('/', (_, res) => {
+  //   res.send('Hi')
+  // })
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(apolloServer)
+  )
+  app.listen(4000, () => {
+    console.log('server started at localhost:4000')
+  })
+  // const post = prisma.post.create({
+  //   data: {
+  //     title: 'new book'
+  //   }
+  // })
+}
 
 main()
   .then(() => {
